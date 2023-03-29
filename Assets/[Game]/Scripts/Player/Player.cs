c3c5264f0ab54204705c4242d89eec09b4abfd66
Mert <mertvanlioglu1337@gmail.com>
2023-03-29 22:18:59 +0300

    Add Player.cs

---
+++ Assets/[Game]/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public Rigidbody Rigidbody => _rigidbody == null ? GetComponent<Rigidbody>() : _rigidbody;
+
+    public bool IsControlable {  get; private set; }
+
+    private void OnEnable()
+    {
+        LevelSystem.Instance.OnLevelStarted.AddListener(Initialize);
+    }
+
+    private void OnDisable()
+    {
+        LevelSystem.Instance.OnLevelStarted.RemoveListener(Initialize);
+    }
+
+    private void Initialize()
+    {
+        IsControlable = true;
+    }
+}
