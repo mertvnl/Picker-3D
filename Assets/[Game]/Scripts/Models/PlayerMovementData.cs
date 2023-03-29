2fc0ab5a94ffabe12c00168348432504403d33c7
Mert <mertvanlioglu1337@gmail.com>
2023-03-29 22:18:43 +0300

    Add Player movement data

---
+++ Assets/[Game]/Scripts/Models/PlayerMovementData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerMovementData", menuName = "Scriptable Objects/Player/MovementData")]
+public class PlayerMovementData : ScriptableObject
+{
+    [field: SerializeField] public float BaseMovementSpeed { get; private set; } = 5f;
+    [field: SerializeField] public float BaseSwerveSpeed { get; private set; } = 3f;
+    [field: SerializeField] public float ClampValueX { get; private set; } = 1.5f;
+}
