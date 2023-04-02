b673945ac3f066cb2f28be04347000af8a2cd7b0
mertvanlioglutrifles <mertvanlioglu1337@gmail.com>
2023-04-02 04:53:49 +0300

    Update LevelData.cs

--- Assets/[Game]/Scripts/Runtime/Models/ScriptableObjects/LevelData.cs
+++ Assets/[Game]/Scripts/Runtime/Models/ScriptableObjects/LevelData.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 [CreateAssetMenu(menuName = "Scriptable Objects/Database/New Level", fileName = "New Level")]
 public class LevelData : ScriptableObject
 {
-    [field: SerializeField] public List<LevelObjectData> LevelObjects { get; private set; } = new List<LevelObjectData>();
+    [field: SerializeField, ReadOnly] public List<LevelObjectData> LevelObjects { get; private set; } = new List<LevelObjectData>();
     [field: SerializeField] public GameObject LevelPrefab { get; private set; }
     [ShowInInspector] public float LevelLength => GetLevelSize();
 
@@ -34,26 +31,4 @@ public class LevelData : ScriptableObject
     {
         LevelPrefab = prefab;
     }
-
-#if UNITY_EDITOR
-    public void CreateLevelPrefab()
-    {
-        //if (LevelPrefab != null)
-            
-
-
-
-        //GameObject levelGameObject = new GameObject(name);
-
-        //float offset = 0f;
-
-        //foreach (LevelObjectData levelObject in LevelObjects)
-        //{
-            
-        //    offset += levelObject.ObjectSize;
-        //}
-
-        //PrefabUtility.SaveAsPrefabAssetAndConnect();
-    }
-#endif
 }
